b33aca908fa5b49a05bf5a0c3e2af40fa0e080d6
pfbrodon <pfbrodon@gmail.com>
2024-02-06 10:12:58 -0300

    models pedido

--- stock/models.py
+++ stock/models.py
@@ -29,5 +29,24 @@ class Producto(models.Model):
     utilidad = models.ForeignKey(Utilidad, on_delete=models.CASCADE)
     precioPublico= models.FloatField(default=0.0)
 
+class Pedidos(models.Model):
+    pedioNum = models.IntegerField(default=0, auto_created=True)
+    pedidoDescripcion= models.CharField(max_length=100)
+    pedidoFecha = models.DateTimeField(auto_now_add=True)
+    totalPedido= models.FloatField(default=0.0)
+
+
+class Productopedido(models.Model):
+    numpedido = models.ForeignKey(Pedidos, on_delete=models.CASCADE)
+    categoria = models.ForeignKey(Categorias, on_delete=models.CASCADE)
+    marca = models.ForeignKey(Marcas, on_delete=models.CASCADE)
+    codigo = models.IntegerField()
+    descripcion = models.CharField(max_length = 150)
+    cantidad = models.IntegerField()
+    precioPublico= models.FloatField(default=0.0)
+    subtotalPedido= models.FloatField(default=0.0)
+
+
+
 def __str__(self):
         return self.title +'-by '+ self.user.username #muestra el titulo de la tarea en la administracion de django
