53b8d79c310f3ce229cc57129fd929c00f95f7ae
pfbrodon <pfbrodon@gmail.com>
2024-02-09 00:43:27 -0300

    va de nuevo

--- buloneria/settings.py
+++ buloneria/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-u@1gp*4jph!n^^hci9rxq@ls92ho*unxt)5me^x7^b00$2a$1&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['djangobuloneriapedidos-production.up.railway.app','[*]']
+ALLOWED_HOSTS = ['djangobuloneriapedidos-production.up.railway.app','127.0.0.1']
 
 CORS_ALLOWED_ORIGINS = [
     "https://djangobuloneriapedidos-production.up.railway.app",
