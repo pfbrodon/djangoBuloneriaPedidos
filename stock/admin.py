76ad0dad7f3a7bc0ff8b4b7c35bc0b0e7d677b71
pfbrodon <pfbrodon@gmail.com>
2024-04-03 15:49:15 -0300

    mal

--- stock/admin.py
+++ stock/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from .models import Producto, Categorias, Utilidad, Marcas
+from .models import Producto, Categorias, Utilidad, Marcas, Pedidosnumtest, Pedidostest
 # Register your models here.
 
 admin.site.register(Producto)
 admin.site.register(Categorias)
 admin.site.register(Utilidad)
 admin.site.register(Marcas)
+admin.site.register(Pedidostest)
+admin.site.register(Pedidosnumtest)
+
+
