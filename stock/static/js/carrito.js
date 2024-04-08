a8baefc2a476cf5d3f06911d38b489949b79bf16
pfbrodon <pfbrodon@gmail.com>
2024-04-08 12:14:31 -0300

    ok

--- stock/static/js/carrito.js
+++ stock/static/js/carrito.js
@@ -1,13 +1,15 @@
-// Obtener referencia al botón y al contenedor del carrito
-var toggleCartBtn = document.getElementById('toggle-cart-btn');
-var carritoContainer = document.getElementById('carrito-container');
+document.addEventListener('DOMContentLoaded', function() {
+    // Obtener referencia al botón y al contenedor del carrito
+    var toggleCartBtn = document.getElementById('toggle-cart-btn');
+    var carritoContainer = document.getElementById('carrito-container');
 
-// Agregar evento de clic al botón
-toggleCartBtn.addEventListener('click', function() {
-    // Si el carrito está visible, ocultarlo; de lo contrario, mostrarlo
-    if (carritoContainer.style.display === 'block') {
-        carritoContainer.style.display = 'none';
-    } else {
-        carritoContainer.style.display = 'block';
-    }
+    // Agregar evento de clic al botón
+    toggleCartBtn.addEventListener('click', function() {
+        // Si el carrito está visible, ocultarlo; de lo contrario, mostrarlo
+        if (carritoContainer.style.display === 'block') {
+            carritoContainer.style.display = 'none';
+        } else {
+            carritoContainer.style.display = 'block';
+        }
+    });
 });
